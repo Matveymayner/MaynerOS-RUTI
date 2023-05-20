b891d4b80f59164ffab7c4d72da0a96cdbccaf4f
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-20 22:46:39 +0300

    MaynerOS-RUTI.lua

--- MaynerOS-RUTI.lua
+++ MaynerOS-RUTI.lua
@@ -1,88 +1,102 @@
 local component = require("component")
-local computer = require("computer")
 local event = require("event")
-local term = require("term")
 local gpu = component.gpu
+local computer = require("computer")
+local os = require("os")
 
 -- Функция для вывода сообщения на экран
 local function message(str)
   gpu.setForeground(0xFFFFFF)
   gpu.setBackground(0x000000)
-  term.clear()
-  term.setCursor(1, 1)
-  print(str)
+  gpu.fill(1, 1, 80, 25, " ")
+  gpu.set(1, 1, str)
 end
 
--- Функция для получения случайного числа
-local function random()
-  return math.random(1, 100)
-end
-
--- Функция для удаления операционной системы
-local function deleteOS()
-  message("Are you sure you want to delete the OS? (y/n)")
-  local _, _, _, _, _, response = event.pull("key_down")
-  if response == 21 then
-    os.remove("/MaynerOS-RUTI.lua")
-    os.exit()
-  else
-    message("OS delete aborted.")
-  end
-end
-
--- Функция для вывода строки команд
-local function printCommands()
-  message("1. shutdown  2. reboot  3. random number  4. delete OS  5. BSOD  6. eternal shutdown")
-end
-
--- Функция для вечного выключения
-local function eternalShutdown()
-  message("Are you sure you want to shutdown the computer? (y/n)")
-  while true do
-    local _, _, _, _, _, response = event.pull("key_down")
-    if response == 21 then
-      computer.shutdown()
-    elseif response == 49 then
-      message("Shutdown aborted.")
-      return
-    end
-  end
+-- Функция для вывода кнопки
+local function drawButton(x, y, width, height, text, foreground, background)
+  gpu.setForeground(foreground)
+  gpu.setBackground(background)
+  gpu.fill(x, y, width, height, " ")
+  local textX = x + math.floor((width - #text) / 2)
+  local textY = y + math.floor(height / 2)
+  gpu.set(textX, textY, text)
 end
 
 -- Функция для обработки команд
 local function handleCommand(command)
   if command == "1" then
+    message("Shutting down...")
+    os.sleep(2)
     computer.shutdown()
   elseif command == "2" then
+    message("Rebooting...")
+    os.sleep(2)
     computer.shutdown(true)
   elseif command == "3" then
-    message("Random number: " .. random())
+    message("Random number: " .. tostring(math.random(1, 100)))
   elseif command == "4" then
-    deleteOS()
-  elseif command == "5" then
-    gpu.setForeground(0xFFFFFF)
-    gpu.setBackground(0x0000FF)
-    gpu.fill(1, 1, 80, 25, " ")
-    gpu.set(32, 12, "Your PC is dead. Sorry :(")
-    gpu.setBackground(0x000000)
-    while true do
-      os.sleep(1)
+    message("Are you sure you want to delete the OS? (y/n)")
+    local _, _, _, _, _, response = event.pull("key_down")
+    if response == 21 then
+      os.remove("/os.lua")
+      os.exit()
+    else
+      message("OS delete aborted.")
+      os.sleep(2)
     end
+  elseif command == "5" then
+    message("Blue Screen of Death!")
+    os.sleep(1)
+    computer.shutdown(true)
   elseif command == "6" then
-    eternalShutdown()
+    message("Are you sure you want to shutdown the computer? (y/n)")
+    while true do
+      local _, _, _, _, _, response = event.pull("key_down")
+      if response == 21 then
+        message("Shutting down...")
+        os.sleep(2)
+        computer.shutdown()
+      elseif response == 49 then
+        message("Shutdown aborted.")
+        os.sleep(2)
+        break
+      end
+    end
   else
     message("Invalid command.")
+    os.sleep(2)
   end
 end
 
--- Очищаем экран и выводим строку команд
-message("Press a key to display the commands.")
-event.pull("key_down")
-printCommands()
+-- Очищаем экран
+gpu.setForeground(0xFFFFFF)
+gpu.setBackground(0x000000)
+gpu.fill(1, 1, 80, 25, " ")
 
--- Ожидаем ввод команды
+-- Выводим кнопки
+drawButton(25, 8, 30, 3, "Shutdown", 0xFFFFFF, 0x555555)
+drawButton(25, 12, 30, 3, "Reboot", 0xFFFFFF, 0x555555)
+drawButton(25, 16, 30, 3, "Random Number", 0xFFFFFF, 0x555555)
+drawButton(25, 20, 30, 3, "Delete OS", 0xFFFFFF, 0x555555)
+drawButton(25, 24, 30, 3, "Blue Screen", 0xFFFFFF, 0x555555)
+drawButton(25, 28, 30, 3, "Eternal Shutdown", 0xFFFFFF, 0x555555)
+
+-- Ожидаем нажатия кнопки
 while true do
-  local _, _, _, _, _, response = event.pull("key_down")
-  handleCommand(tostring(response - 2))
-  printCommands()
+  local _, _, x, y = event.pull("touch")
+  if x >= 25 and x <= 54 then
+    if y == 8 then
+      handleCommand("1")
+    elseif y == 12 then
+      handleCommand("2")
+    elseif y == 16 then
+      handleCommand("3")
+    elseif y == 20 then
+      handleCommand("4")
+    elseif y == 24 then
+      handleCommand("5")
+    elseif y == 28 then
+      handleCommand("6")
+    end
+  end
 end
